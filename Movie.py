aeb807284c8cf7011442b26dd34d8bb772a20c5b
shreya123093 <62276610+shreya123093@users.noreply.github.com>
2022-05-06 17:04:20 +0530

    Update Movie.py

--- Movie.py
+++ Movie.py
@@ -2,7 +2,7 @@ import mysql.connector
 mydb=mysql.connector.connect(
         host="localhost",
         user="root",
-        password="1239"
+        password="2001"
         )
 mycursor=mydb.cursor()
 mycursor.execute("SHOW DATABASES")
